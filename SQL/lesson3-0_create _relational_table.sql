aaefb9ef17f6d99173e1d7dd042a8324ea625a90
曾意晴 <kmi8610@gmail.com>
2024-03-09 10:29:03 +0800

    Update lesson3-0_create _relational_table.sql

--- SQL/lesson3-0_create _relational_table.sql
+++ SQL/lesson3-0_create _relational_table.sql
@@ -1,4 +1,5 @@
 DROP table if exists customers;
+DROP table if exists contact;
 
 create table if not exists customers(
 	customer_id serial,
@@ -16,4 +17,11 @@ create table if not exists contact(
 	foreign key(customer_id) references customers(customer_id)
 );
 
-DROP table if exists contact
+
+insert into customers(customer_name)
+values ('Disney+'),('Netflix'),('Hami')
+
+insert into contact(customer_id, contact_name, contact_phone, contact_mail)
+values  (1, 'Mr.A', '(03)5641-4152', 'aaa@abc.com'),
+		(1, 'Mr.B', '(08)5124-8534', 'bbb@abc.com'),
+		(2, 'Ms.C', '(04)0032-9154', 'ccc@abc.com');
