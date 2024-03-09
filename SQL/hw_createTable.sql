52d4ab7716788c5967c086fbd085a70c2a8f6c94
曾意晴 <kmi8610@gmail.com>
2024-03-09 11:40:03 +0800

    join
    語法練習

--- SQL/hw_createTable.sql
+++ SQL/hw_createTable.sql
@@ -3,13 +3,13 @@ CREATE TABLE IF NOT EXISTS invoices(
 	CustomerId INT,
 	InvoiceDate TIMESTAMP,
 	BillingAddress VARCHAR,
-	BillingCity VARCHAR(30),
-	BillingState VARCHAR(30),
-	BillingCountry VARCHAR(30),
-	BillingPostalCode VARCHAR(30),
-	Total FLOAT
+	BillingCity VARCHAR(300),
+	BillingState VARCHAR(300),
+	BillingCountry VARCHAR(300),
+	BillingPostalCode VARCHAR(300),
+	Total FLOAT8
 );
 
-SELECT * FROM invoices
+SELECT * FROM invoices;
 
 drop table invoices
--- SQL/lesson3-0_create _relational_table.sql
+++ SQL/lesson3-0_create _relational_table.sql
@@ -1,6 +1,9 @@
 DROP table if exists customers;
 DROP table if exists contact;
 
+select * from customers;
+select * from contact;
+
 create table if not exists customers(
 	customer_id serial,
 	customer_name varchar(50),
@@ -15,6 +18,7 @@ create table if not exists contact(
 	contact_mail varchar(100),
 	primary key(contact_id),
 	foreign key(customer_id) references customers(customer_id)
+	on delete set null 
 );
 
 
@@ -25,3 +29,10 @@ insert into contact(customer_id, contact_name, contact_phone, contact_mail)
 values  (1, 'Mr.A', '(03)5641-4152', 'aaa@abc.com'),
 		(1, 'Mr.B', '(08)5124-8534', 'bbb@abc.com'),
 		(2, 'Ms.C', '(04)0032-9154', 'ccc@abc.com');
+
+delete from customers
+where customer_id = 1;
+
+select contact_id, customer_name,contact_name, contact_phone, contact_mail 
+from contact left join customers on contact.customer_id=customers.customer_id
+where customer_name = 'Disney+'
