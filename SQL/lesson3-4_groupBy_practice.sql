8de197490b6ce63e17246d0ea69081d8b71701c4
曾意晴 <kmi8610@gmail.com>
2024-03-09 21:52:15 +0800

    Update lesson3-4_groupBy_practice.sql

--- SQL/lesson3-4_groupBy_practice.sql
+++ SQL/lesson3-4_groupBy_practice.sql
@@ -11,14 +11,38 @@ group by stationName
 having sum(gateInComing)> 5000000;
 
 /*基隆火車站2020年,每月份進站人數*/
-select to_month(trnDate,) as month, sum(gateInComing) as totalEntranceNum from stationNum
+select extract(month from trnDate) as month, sum(gateInComing) as totalEntranceNum from stationNum
+left join station using (stationCode)
+where stationName = '基隆'
+and trnDate between '2020-01-01' and '2020-12-31'
+group by month;
+
+/*基隆火車站2020年,每月份進站人數,由多至少*/
+select extract(month from trnDate) as month, sum(gateInComing) as totalEntranceNum from stationNum
 left join station using (stationCode)
 where stationName = '基隆'
 and trnDate between '2020-01-01' and '2020-12-31'
 group by month
-
-/*基隆火車站2020年,每月份進站人數,由多至少*/
-
+order by totalEntranceNum desc;
 
 /*基隆火車站2020,2021,2022,每年進站人數*/
+select extract(year from trnDate) as year, sum(gateInComing) as totalEntranceNum from stationNum
+left join station using (stationCode)
+where stationName = '基隆'
+and trnDate between '2020-01-01' and '2022-12-31'
+group by year;
+
 /*基隆火車站,臺北火車站2020,2021,2022,每年進站人數*/
+/*total*/
+select extract(year from trnDate) as year, sum(gateInComing) as totalEntranceNum from stationNum
+left join station using (stationCode)
+where stationName in ('基隆', '臺北')
+and trnDate between '2020-01-01' and '2022-12-31'
+group by year;
+/*seperate*/
+select extract(year from trnDate) as year, stationName, sum(gateInComing) as totalEntranceNum from stationNum
+left join station using (stationCode)
+where stationName in ('基隆', '臺北')
+and trnDate between '2020-01-01' and '2022-12-31'
+group by year, stationName
+order by year;
